f926bee116d95ab45f457589df2d62b05f847a8d
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-06-28 06:32:43 +0300

    control flow with loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        if i == 1:
+            print(f"{i}")
+            print("Happy New Year!")
+            i -= 1
+        else:
+            print(f"{i}")
+            i -= 1
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = [integer * integer for integer in int_list]
+    return squared_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i <= 100:
+        if i % 15 == 0:
+            print("FizzBuzz")
+            i += 1
+        elif i % 5 == 0:
+            print("Buzz")
+            i += 1
+        elif i % 3 == 0:
+            print("Fizz")
+            i += 1
+        else:
+            print(f"{i}")
+            i += 1
